99a5eff83f6bd278e1ad8459026107fa1c493acb
yejcs <singer0015@gmail.com>
2022-05-09 10:16:13 +0900

    test1

---
+++ Pods/Target Support Files/Pods-testCalendarTests/Pods-testCalendarTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testCalendarTests : NSObject
+@end
+@implementation PodsDummy_Pods_testCalendarTests
+@end
--- testCalendar/ViewController.swift
+++ testCalendar/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import FSCalendar
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calendarView: FSCalendar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calendarView.backgroundColor = UIColor(red:241/255, green: 249/255, blue: 255/255, alpha: 1)
     }
 
 
